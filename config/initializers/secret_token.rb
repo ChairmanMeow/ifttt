da6987409c2ed3ebd3f4d1f6aa79d557c5b9a0cc
Victor Wong <turtlecloud@gmail.com>
2013-06-22 13:35:50 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IFTTT::Application.config.secret_token = 'cc0efaa24dc5d2482985eb902ecaaf0c6646e5b961c4237bc57f2ddbc9b6f6834b22b1f755dc742cd11c0d09ed7fd2febabd1dae029295476d29147a40709487'
