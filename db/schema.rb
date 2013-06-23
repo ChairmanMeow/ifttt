ec7cbc13ce033c12830d50fcebabefabf95b21d9
Victor Wong <turtlecloud@gmail.com>
2013-06-23 00:17:09 -0700

    Defined Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130622205519) do
+ActiveRecord::Schema.define(:version => 20130623071221) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
